e760e000a7636bacd48dcf4fcd08c0f6c8631917
Suryansh Saxena <suryanshctf05@gmail.com>
2025-02-08 12:55:14 +0530

    By mistake exposed apikeys🫣🫶😳👉👈

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,15 +1,14 @@
-// lib/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSFEnLXzuqel1g15If_UMtGZXsVnxwPzw",
-  authDomain: "iitrsba.firebaseapp.com",
-  projectId: "iitrsba",
-  storageBucket: "iitrsba.firebasestorage.app",
-  messagingSenderId: "393673240182",
-  appId: "1:393673240182:web:fa2d953fc9a1c366737957",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
